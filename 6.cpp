f8795101f76655cd43067c1584674597760fdfdc
SunakshiRawat <sam13army@gmail.com>
2024-11-24 21:43:01 +0530

    6.cpp

--- 6.cpp
+++ 6.cpp
@@ -1,3 +1,5 @@
+// Display the sum of two numbers using multilevel inheritance where one number is declared in base class and one number declared in first derived class. Function is defined in the second derived class to sum two numbers. 
+
 #include<iostream>
 using namespace std;
 
